a8b66eaf85e6115f310a1290985e0b6edd71f2dd
goerge32 <goerge32@opentrash.com>
2020-07-02 18:22:02 +0430

    adding slug

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.articlesList),
+    path('slug',views.article_detail)
 ]
 
--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from . import models
 # Create your views here.
 def articlesList(request):
     articles = models.Article.objects.all().order_by('date')
 
-
-
     return render(request, 'articles/articleslist.html',{'articles':articles})
     
-
+def article_datail(request, slug):
+    return HttpResponse(slug)
